eef5b2991d9c3ee9689b8047e6b5ce9d49429672
Simon Dawe <simon.dawe@oodlefinance.com>
2022-03-10 18:18:53 +0000

    add tests

--- src/test/groovy/io/github/smdawe/WiremockExamples.groovy
+++ src/test/groovy/io/github/smdawe/WiremockExamples.groovy
@@ -1,6 +1,7 @@
 package io.github.smdawe
 
 import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.common.Slf4jNotifier
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration
 import com.github.tomakehurst.wiremock.http.Fault
 import groovy.json.JsonOutput
@@ -9,7 +10,6 @@ import spock.lang.Shared
 import spock.lang.Specification
 
 import java.net.http.HttpClient
-import java.net.http.HttpHeaders
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 import java.time.Duration
@@ -19,7 +19,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*
 class WiremockExamples extends Specification {
 
   @Shared
-  WireMockServer mockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort())
+  WireMockServer mockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort().notifier(new Slf4jNotifier(true)))
 
   @Shared
   String baseUrl
@@ -46,77 +46,98 @@ class WiremockExamples extends Specification {
 
   void 'http get to a JSON api'() {
     given:
-      Map<String, String> responseBody = [hello: 'world']
-      mockServer.stubFor(get(testPath).willReturn(okJson(JsonOutput.toJson(responseBody))))
+    Map<String, String> responseBody = [hello: 'world']
+    mockServer.stubFor(get(testPath).willReturn(okJson(JsonOutput.toJson(responseBody))))
 
-      HttpRequest request = HttpRequest.newBuilder()
+    HttpRequest request = HttpRequest.newBuilder()
             .uri(new URI(testUrl))
             .GET()
             .build()
     when:
-      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString())
+    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString())
 
     then:
-      response.statusCode() == 200
-      response.headers().firstValue('Content-Type').get() == 'application/json'
-      new JsonSlurper().parseText(response.body()) == responseBody
+    response.statusCode() == 200
+    response.headers().firstValue('Content-Type').get() == 'application/json'
+    new JsonSlurper().parseText(response.body()) == responseBody
+  }
+
+  void 'http post to a JSON api'() {
+    given:
+    Map<String, String> postBody = [hello: 'world']
+    mockServer.stubFor(post(testPath)
+            .withHeader('Content-Type', equalTo('application/json'))
+            .withRequestBody(equalTo(JsonOutput.toJson(postBody)))
+            .willReturn(created())
+    )
+
+    HttpRequest request = HttpRequest.newBuilder()
+            .uri(new URI(testUrl))
+            .POST(HttpRequest.BodyPublishers.ofString(JsonOutput.toJson(postBody)))
+            .header('Content-Type', 'application/json')
+            .build()
+    when:
+    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+    then:
+    response.statusCode() == 201
   }
 
   void 'http get to a JSON api - simulate fault'() {
     given:
-      mockServer.stubFor(get(testPath).willReturn(aResponse().withFault(Fault.CONNECTION_RESET_BY_PEER)))
+    mockServer.stubFor(get(testPath).willReturn(aResponse().withFault(Fault.CONNECTION_RESET_BY_PEER)))
 
-      HttpRequest request = HttpRequest.newBuilder()
-              .uri(new URI(testUrl))
-              .GET()
-              .build()
+    HttpRequest request = HttpRequest.newBuilder()
+            .uri(new URI(testUrl))
+            .GET()
+            .build()
     when:
-      client.send(request, HttpResponse.BodyHandlers.ofString())
+    client.send(request, HttpResponse.BodyHandlers.ofString())
 
     then:
-      Exception e = thrown(IOException)
-      e.message == 'Connection reset by peer'
+    Exception e = thrown(IOException)
+    e.message == 'Connection reset by peer'
   }
 
   void 'http get to a JSON api - timeout'() {
     given:
-      int delay = 1_000
-      Duration timeout = Duration.ofMillis(500)
-      Map<String, String> responseBody = [hello: 'world']
-      mockServer.stubFor(get(testPath).willReturn(okJson(JsonOutput.toJson(responseBody)).withFixedDelay(delay)))
+    int delay = 1_000
+    Duration timeout = Duration.ofMillis(500)
+    Map<String, String> responseBody = [hello: 'world']
+    mockServer.stubFor(get(testPath).willReturn(okJson(JsonOutput.toJson(responseBody)).withFixedDelay(delay)))
 
-      HttpRequest request = HttpRequest.newBuilder()
-              .uri(new URI(testUrl))
-              .GET()
-              .timeout(timeout)
-              .build()
+    HttpRequest request = HttpRequest.newBuilder()
+            .uri(new URI(testUrl))
+            .GET()
+            .timeout(timeout)
+            .build()
     when:
-      client.send(request, HttpResponse.BodyHandlers.ofString())
+    client.send(request, HttpResponse.BodyHandlers.ofString())
 
     then:
-      Exception e = thrown(IOException)
-      e.message == 'request timed out'
+    Exception e = thrown(IOException)
+    e.message == 'request timed out'
   }
 
   void 'http get to a JSON api - delay'() {
     given:
-      int delay = 500
-      Duration timeout = Duration.ofMillis(1000)
-      Map<String, String> responseBody = [hello: 'world']
-      mockServer.stubFor(get(testPath).willReturn(okJson(JsonOutput.toJson(responseBody)).withFixedDelay(delay)))
+    int delay = 500
+    Duration timeout = Duration.ofMillis(1000)
+    Map<String, String> responseBody = [hello: 'world']
+    mockServer.stubFor(get(testPath).willReturn(okJson(JsonOutput.toJson(responseBody)).withFixedDelay(delay)))
 
-      HttpRequest request = HttpRequest.newBuilder()
-              .uri(new URI(testUrl))
-              .GET()
-              .timeout(timeout)
-              .build()
+    HttpRequest request = HttpRequest.newBuilder()
+            .uri(new URI(testUrl))
+            .GET()
+            .timeout(timeout)
+            .build()
     when:
-      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString())
+    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString())
 
     then:
-      response.statusCode() == 200
-      response.headers().firstValue('Content-Type').get() == 'application/json'
-      new JsonSlurper().parseText(response.body()) == responseBody
+    response.statusCode() == 200
+    response.headers().firstValue('Content-Type').get() == 'application/json'
+    new JsonSlurper().parseText(response.body()) == responseBody
   }
 
 }
--- src/test/groovy/io/github/smdawe/WiremockGqlExamples.groovy
+++ src/test/groovy/io/github/smdawe/WiremockGqlExamples.groovy
@@ -1,29 +1,22 @@
 package io.github.smdawe
 
 import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.common.Slf4jNotifier
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration
-import com.github.tomakehurst.wiremock.http.Fault
 import com.netflix.graphql.dgs.client.GraphQLResponse
 import com.netflix.graphql.dgs.client.MonoGraphQLClient
 import com.netflix.graphql.dgs.client.WebClientGraphQLClient
-import groovy.json.JsonOutput
-import groovy.json.JsonSlurper
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 import spock.lang.Shared
 import spock.lang.Specification
-
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
-import java.time.Duration
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*
 
 class WiremockGqlExamples extends Specification {
 
   @Shared
-  WireMockServer mockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort())
+  WireMockServer mockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort().notifier(new Slf4jNotifier(true)))
 
   @Shared
   String baseUrl
@@ -45,9 +38,8 @@ class WiremockGqlExamples extends Specification {
   void setup() {
     testPath = '/graphql'
     testUrl = "$baseUrl$testPath"
-    WebClient webClient = WebClient.create("http://localhost:8080/graphql");
+    WebClient webClient = WebClient.create(testUrl);
     client = MonoGraphQLClient.createWithWebClient(webClient);
-
   }
 
   void 'graphql'() {
@@ -59,13 +51,29 @@ class WiremockGqlExamples extends Specification {
     field2
   }
 }
-      """
+"""
+      String response = """
+{
+  "data": {
+    "field1":"test1",
+    "field2":"test2"
+  }
+}
+"""
+      String actualQuery = '{"query":"\\n{\\n  example(id: \\"1\\") {\\n    field1\\n    field2\\n  }\\n}\\n","variables":{},"operationName":null}'
+
+      mockServer.stubFor(post(testPath)
+        .withHeader("Content-Type", equalTo("application/json"))
+        .withRequestBody(equalTo(actualQuery))
+        .willReturn(okJson(response))
+      );
+
     when:
       Mono<GraphQLResponse> graphQLResponseMono = client.reactiveExecuteQuery(query);
     then:
-      Mono<String> field = graphQLResponseMono.map(r -> r.extractValue("field1"));
-      field.subscribe(result -> println(result))
-      Thread.sleep(1000)
+      Mono<String> field1 = graphQLResponseMono.map(r -> r.extractValue("field1"));
+      field1.subscribe()
+      field1.block() == "test1"
   }
 
 }
